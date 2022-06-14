10d36e0611e82cde460693d9d400f3964f7d85e7
KalininaEkaterina <79200863+KalininaEkaterina@users.noreply.github.com>
2022-06-14 18:37:37 +0300

    add

--- post/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Nursery(models.Model):
-    name = models.CharField('Название', max_length=50)
-    descr = models.TextField('Описание')
-
-    def str(self):
-        return self.name
